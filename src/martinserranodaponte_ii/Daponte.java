ab9ce2be9afa9c764ac99aadff9ffbc6bdb38c04
Julián Martín-Serrano Daponte <2037468@alu.murciaeduca.es>
2022-06-04 13:00:23 +0200

    1ª Versión. Se introduce en el repositorio local el proyecto Java MartinSerranoDaponte_II, el cual contiene la clase principal con el main del mismo nombre y dos clases más, una llamada MartinSerrano y otra llamada Daponte. Ninguna de ellas contiene código más que el generado por Netbeans

---
+++ src/martinserranodaponte_ii/Daponte.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package martinserranodaponte_ii;
+
+/**
+ *
+ * @author Julián
+ */
+public class Daponte {
+    
+}
